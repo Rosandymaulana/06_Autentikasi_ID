a14796f34e63f81e4725d9db0b1683dbeb4b22f0
Rosandymaulana <rosandy1118@gmail.com>
2022-03-28 15:21:14 +0700

    Praktikum 3: Change login using  username

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -24,7 +24,7 @@ class LoginController extends Controller
     /**
      * Where to redirect users after login.
      *
-     * @var string
+     * @var string-
      */
     protected $redirectTo = RouteServiceProvider::HOME;
 
@@ -36,5 +36,10 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function username()
+    {
+        return 'username';
     }
 }
